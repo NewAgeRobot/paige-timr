0e10c1ac983a997ddb86daa061640e0b9dd97c76
NewAgeRobot <seanmcdonnell90@gmail.com>
2021-01-31 21:04:36 +0000

    using bulma css removed scrollbars wrapped state tracker in if statement for idling precursor to prevent jumping states set up timer div set up pause functionality on timer - unpause time fix still not working

--- src/render.js
+++ src/render.js
@@ -1,8 +1,18 @@
 var modeState = "";
-document.getElementById("stateBox").innerHTML = "Idling";
+var timerActive = "";
+var pauseState = new Boolean(0);
+var start;
+var pauseTime = 0;
+var delta;
+var time;
+var seconds;
+var minutes;
+var hours;
 
 window.addEventListener("keydown", checkKeyPressed, false);
 function checkKeyPressed(evt) {
+//RECORDING STATES
+  if(modeState == ""){              //to prevent multi-button pressing jumping states
     if (evt.keyCode == "90") {      //key Z
         modeState = "College";
     }
@@ -15,14 +25,74 @@ function checkKeyPressed(evt) {
     else if (evt.keyCode == "65") { //key A
         modeState = "Leisure";
     }
+  }
+
+  //? adding for testing purposes to "unclick" switch/reset state
+  else if (evt.keyCode == "13") { //key A
+      modeState = "";
+  }
+
+  //RECORDING SUB COMMANDS
+  if (modeState !== ""){
+    //START TIMER
+    if ((evt.keyCode == "49") && (timerActive == "")) {      //key 1
+      start = Date.now();
+      if(modeState !== "" ){
+          timerActive = "active";
+      }
+    }
+    else if ((evt.keyCode == "50") && ((timerActive = "active")||(timerActive = "pause"))) {      //key 2
+      if(pauseState == 0){
+        pauseTime = Date.now();
+        pauseState = 1;
+      }
+      else if(pauseState == 1){
+        pauseState = 0;
+      }
+      timerActive = "pause";
+    }
+
+    else if ((evt.keyCode == "51") && ((timerActive = "active")||(timerActive = "pause"))) {      //key 2
+      timerActive = "pause";
+      totalTime = document.getElementById("timer").innerHTML();
+    }
+  }
 }
 
 window.addEventListener("keyup", checkKeyUp, false);
 function checkKeyUp(evt) {
-      modeState = "";
+      //! taken out for the moment to test without latch switches
+      //modeState = "";
 }
 
-window.setInterval( function(){
+
+window.setInterval( function(){ //Timer Tracker
+  switch (timerActive) {
+    case "active":
+      document.getElementById("timer").classList.remove('blink');
+      delta = Date.now() - start; // milliseconds elapsed since start
+      time = (Math.floor(delta / 1000));
+      seconds = time % 60 < 10 ? "0"+time % 60: time % 60; // in seconds
+      minutes = Math.floor(time / 60)< 10 ? "0"+Math.floor(time / 60): Math.floor(time / 60);
+      hours = Math.floor(minutes / 60)< 10 ? "0"+Math.floor(minutes / 60): Math.floor(minutes / 60);
+      document.getElementById("timer").innerHTML = hours+':'+minutes+':'+seconds;
+      break;
+    case "pause":
+      if(pauseState == 0){//unpaused state
+        timerActive = "active";
+      }
+      else if(pauseState == 1){//paused state
+        console.log( "paused");
+        document.getElementById("timer").classList.add('blink');
+      }
+      break;
+    case "stop":
+
+      break;
+  }
+},100)
+
+window.setInterval( function(){ //State Tracker
   switch (modeState) {
     case "":
       document.getElementById("stateBox").innerHTML = "Idling";
