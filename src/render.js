b213a7ecdade6e614862e38c20f32257396d87de
NewAgeRobot <seanmcdonnell90@gmail.com>
2021-02-01 00:09:36 +0000

    fixed the issue of only the most recent pauseDiff being deducted from the current timer calculation. Timer pausing works now.

--- src/render.js
+++ src/render.js
@@ -5,6 +5,7 @@ var start;
 var pauseTime = 0;
 var unpauseTime = 0;
 var pauseDiff = 0;
+var pauseDiffTotal = 0;
 var delta;
 var time;
 var seconds;
@@ -73,7 +74,7 @@ window.setInterval( function(){ //Timer Tracker
   switch (timerActive) {
     case "active":
       document.getElementById("timer").classList.remove('blink');
-      delta = (Date.now() - (start+pauseDiff)); // milliseconds elapsed since start
+      delta = (Date.now() - (start+pauseDiffTotal)); // milliseconds elapsed since start
       time = (Math.floor(delta / 1000));
       seconds = time % 60 < 10 ? "0"+time % 60: time % 60; // in seconds
       minutes = Math.floor(time / 60)< 10 ? "0"+Math.floor(time / 60): Math.floor(time / 60);
@@ -82,7 +83,7 @@ window.setInterval( function(){ //Timer Tracker
       break;
     case "pause":
       if(pauseState == 0){//unpaused state
-        pauseDiff = unpauseTime-pauseTime;
+        pauseDiffTotal += (unpauseTime-pauseTime);
         timerActive = "active";
       }
       else if(pauseState == 1){//paused state
