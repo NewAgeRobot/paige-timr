a9dab4d371946a4271c44998bb847b202ff52c9c
NewAgeRobot <seanmcdonnell90@gmail.com>
2021-02-02 22:30:13 +0000

    Got the reading and writing of a basic CSV file implemented.
    Re-arranged the program slightly to have there be a series of 
    subcommands based on which latch switch is activated.
    You then choose the timer functionality from that, and also updated the 
    "reset" functionality of the enter button to end the timer if active - 
    save it out - and make the elements invisible again.

--- src/render.js
+++ src/render.js
@@ -1,4 +1,7 @@
-var modeState = "";
+var fs = require('fs');
+
+var latchState = "";
+var optionState = "";
 var timerActive = "";
 var pauseState = new Boolean(0);
 var start;
@@ -12,57 +15,119 @@ var time;
 var seconds;
 var minutes;
 var hours;
+var currentCSVValue;
+var timerCSVUpdateValue;
+
+var option1 = "";
+var option2 = "";
+var option3 = "";
+var option4 = "";
+var option5 = "";
+
 
 window.addEventListener("keydown", checkKeyPressed, false);
 function checkKeyPressed(evt) {
+  console.log("latch: " + latchState + " option: " + optionState);
 //RECORDING STATES
-  if(modeState == ""){              //to prevent multi-button pressing jumping states
+  if(latchState == ""){             //to prevent multi-button pressing jumping states
     if (evt.keyCode == "90") {      //key Z
-        modeState = "College";
+        latchState = "College";
+        //work in subtasks where necessary
+        option1 = "Timer";
+        option2 = "Subject Pomodoro";
+        option3 = "History";
+        document.getElementById("option1").innerHTML = option1;
+        document.getElementById("option2").innerHTML = option2;
+        document.getElementById("option3").innerHTML = option3;
+        document.getElementById("optionBox").style.visibility = "visible";
     }
     else if (evt.keyCode == "88") { //key X
-        modeState = "Work";
+        latchState = "Work";
+        option1 = "Timer";
+        option2 = "Task Pomodoro";
+        option3 = "History";
+        document.getElementById("option1").innerHTML = option1;
+        document.getElementById("option2").innerHTML = option2;
+        document.getElementById("option3").innerHTML = option3;
+        document.getElementById("optionBox").style.visibility = "visible";
     }
     else if (evt.keyCode == "67") { //key C
-        modeState = "House";
+        latchState = "House";
+        option1 = "Timer";
+        option2 = "Pomodoro";
+        option3 = "History";
+        document.getElementById("option1").innerHTML = option1;
+        document.getElementById("option2").innerHTML = option2;
+        document.getElementById("option3").innerHTML = option3;
+        document.getElementById("optionBox").style.visibility = "visible";
     }
     else if (evt.keyCode == "65") { //key A
-        modeState = "Leisure";
+        latchState = "Leisure";
+        option1 = "Timer";
+        option2 = "Wank Pomodoro";
+        option3 = "History";
+        document.getElementById("option1").innerHTML = option1;
+        document.getElementById("option2").innerHTML = option2;
+        document.getElementById("option3").innerHTML = option3;
+        document.getElementById("optionBox").style.visibility = "visible";
     }
   }
 
   //? adding for testing purposes to "unclick" switch/reset state
-  else if (evt.keyCode == "13") { //key A
-      modeState = "";
+  else if (evt.keyCode == "13") { //key Enter
+      latchState = "";
+      if(timerActive !== ""){
+        timerActive = "stop";
+      }
+      document.getElementById("optionBox").style.visibility = "hidden";
+      document.getElementById("timer").style.visibility = "hidden";
   }
 
-  //RECORDING SUB COMMANDS
-  if (modeState !== ""){
-    if ((evt.keyCode == "49") && (timerActive == "")) {      //key 1 START TIMER
-      if(modeState !== "" ){
-        resetTimer();
-        start = Date.now();
-        timerActive = "active";
-      }
+
+
+  if (latchState !== ""){
+    //program back button
+
+    if ((evt.keyCode == "49") && (timerActive == "")){//cant change at the moment while timer is active - eventually fix to just have it stop the timer and save it out
+      optionState = option1;
     }
-    else if ((evt.keyCode == "50") && ((timerActive == "active")||(timerActive == "pause"))) {      //key 2 PAUSE TIMER
-      if(pauseState == 0){
-        pauseTime = Date.now();
-        pauseState = 1;
-      }
-      else if(pauseState == 1){
-        unpauseTime = Date.now();
-        pauseState = 0;
-      }
-      timerActive = "pause";
+    else if ((evt.keyCode == "50") && (timerActive == "")){
+      optionState = option2;
+    }
+    else if ((evt.keyCode == "51") && (timerActive == "")){
+      optionState = option3;
     }
 
-    else if ((evt.keyCode == "51") && ((timerActive == "active")||(timerActive == "pause"))) {      //key 2 UNPAUSE TIMER
-      timerActive = "stop";
-    }
-  }
+    if (optionState == "Timer"){
+      document.getElementById("timer").style.visibility = "visible";
+      //TIMER SUB COMMANDS
+      if ((evt.keyCode == "49") && (timerActive == "")) {      //key 1 START TIMER
+        if(latchState !== ""){
+          resetTimer();
+          start = Date.now();
+          timerActive = "active";
+        }
+      }
+      else if ((evt.keyCode == "50") && ((timerActive == "active")||(timerActive == "pause"))) {      //key 2 PAUSE TIMER
+        if(pauseState == 0){
+          pauseTime = Date.now();
+          pauseState = 1;
+        }
+        else if(pauseState == 1){
+          unpauseTime = Date.now();
+          pauseState = 0;
+        }
+        timerActive = "pause";
+      }
+
+      else if ((evt.keyCode == "51") && ((timerActive == "active")||(timerActive == "pause"))) {      //key 2 UNPAUSE TIMER
+        timerActive = "stop";
+      }
+    }//Timer optionState end
+  }//latchState subcommand end
 }
 
+//reset the timers and everything so they're fresh for a new timer to start
 function resetTimer(){
   document.getElementById("timer").classList.remove('blink');
   document.getElementById("timer").classList.remove('stoppedTime');
@@ -81,16 +146,56 @@ function resetTimer(){
   totalTime = 0;
 }
 
+//concatenate the value that is being amended into the CSV file
+function timerCSVUpdateCalculation(){
+  fs.readFile('C:/Users/seanm/OneDrive/Desktop/PaigeTimr/paige-timr/TimerData.csv', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    currentCSVValue = data;
+    console.log(currentCSVValue);
+  })
+  let d = new Date()
+  let day = d.getDay();
+  switch (day){
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+  }
+  let currentTimeReading = document.getElementById("timer").innerHTML = hours+':'+minutes+':'+seconds;
+  timerCSVUpdateValue = day + "," + latchState + "," + time + "," + currentTimeReading + "\r\n";
+}
+
 window.addEventListener("keyup", checkKeyUp, false);
 function checkKeyUp(evt) {
       //! taken out for the moment to test without latch switches
-      //modeState = "";
+      //latchState = "";
 }
 
 
 window.setInterval( function(){ //Timer Tracker
   switch (timerActive) {
     case "active":
+      document.getElementById("timer").classList.remove('blink');
       document.getElementById("timer").classList.add('counting');
       delta = (Date.now() - (start+pauseDiffTotal)); // milliseconds elapsed since start
       time = (Math.floor(delta / 1000));
@@ -110,6 +215,11 @@ window.setInterval( function(){ //Timer Tracker
       break;
     case "stop":
       totalTime = time;
+      timerCSVUpdateCalculation();
+      fs.appendFile('C:/Users/seanm/OneDrive/Desktop/PaigeTimr/paige-timr/TimerData.csv',timerCSVUpdateValue,function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
       resetTimer();
       document.getElementById("timer").classList.add('stoppedTime');
       timerActive = "";
@@ -119,25 +229,25 @@ window.setInterval( function(){ //Timer Tracker
 },100)
 
 window.setInterval( function(){ //State Tracker
-  switch (modeState) {
+  switch (latchState) {
     case "":
-      document.getElementById("stateBox").innerHTML = "Idling";
+      document.getElementById("latchBox").innerHTML = "Idling";
     //  document.body.style.backgroundColor = "white";
       break;
     case "College":
-      document.getElementById("stateBox").innerHTML = "Working on College";
+      document.getElementById("latchBox").innerHTML = "College";
     //  document.body.style.backgroundColor = "#AFF8D8";
       break;
     case "Work":
-      document.getElementById("stateBox").innerHTML = "Actually doing Work";
+      document.getElementById("latchBox").innerHTML = "Work";
     //  document.body.style.backgroundColor = "#FFBEBC";
       break;
     case "House":
-      document.getElementById("stateBox").innerHTML = "Working on House Stuff";
+      document.getElementById("latchBox").innerHTML = "House";
     //  document.body.style.backgroundColor = "#D5AAFF";
       break;
     case "Leisure":
-      document.getElementById("stateBox").innerHTML = "Leisure time!";
+      document.getElementById("latchBox").innerHTML = "Leisure";
     //  document.body.style.backgroundColor = "#85E3FF";
       break;
   }
