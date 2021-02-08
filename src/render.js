fb11cf74abfd1551ca39216477f75776e5751ca4
NewAgeRobot <seanmcdonnell90@gmail.com>
2021-02-08 16:35:12 +0000

    Added in rest of Asana array Pulldowns - separated labels turning on into separate function

--- src/render.js
+++ src/render.js
@@ -1,26 +1,52 @@
 var fs = require('fs');
 const asana = require('asana');
-var asanaCollegeSectionsGrab = [];
-var asanaCollegeSectionsArray = [];
-var asanaCollegeTasksArray = [];
 var asanaCollegeArray = [];
+var asanaWorkArray = [];
+var asanaHomeArray = [];
+var asanaLifeArray = [];
 //auth token - 1/1199906203295061:3e0be57da5c97ebc3ee5d20ec409e418
 
 const client = asana.Client.create().useAccessToken('1/1199906203295061:3e0be57da5c97ebc3ee5d20ec409e418');
 
-//function onlyUnique(value, index, self) {
-//  return self.indexOf(value) === index;
-//}
-//_ = _.filter(onlyUnique);
-
+//College Array Pulldown
 client.tasks.getTasksForProject('1199906289002007', {param: "value", param: "value", opt_pretty: true, opt_fields: 'name, memberships.section.name'})
-    .then((result) => {
-      console.log(result);
-          for(var taskCounter = 0; taskCounter < result.data.length; taskCounter++){
-            asanaCollegeArray.push([result.data[taskCounter].memberships[0].section.name,result.data[taskCounter].name]);
-          }
-        console.table(asanaCollegeArray);
-    });
+  .then((result) => {
+    console.log(result);
+        for(var taskCounter = 0; taskCounter < result.data.length; taskCounter++){
+          asanaCollegeArray.push([result.data[taskCounter].memberships[0].section.name,result.data[taskCounter].name]);
+        }
+      console.table(asanaCollegeArray);
+  });
+
+//Work Array Pulldown
+client.tasks.getTasksForProject('1199909235624453', {param: "value", param: "value", opt_pretty: true, opt_fields: 'name, memberships.section.name'})
+  .then((result) => {
+    console.log(result);
+        for(var taskCounter = 0; taskCounter < result.data.length; taskCounter++){
+          asanaWorkArray.push([result.data[taskCounter].memberships[0].section.name,result.data[taskCounter].name]);
+        }
+      console.table(asanaWorkArray);
+  });
+
+//Home Array Pulldown
+client.tasks.getTasksForProject('1199909235624471', {param: "value", param: "value", opt_pretty: true, opt_fields: 'name, memberships.section.name'})
+  .then((result) => {
+    console.log(result);
+        for(var taskCounter = 0; taskCounter < result.data.length; taskCounter++){
+          asanaHomeArray.push([result.data[taskCounter].memberships[0].section.name,result.data[taskCounter].name]);
+        }
+      console.table(asanaHomeArray);
+  });
+
+//Life Array Pulldown
+client.tasks.getTasksForProject('1199909235624487', {param: "value", param: "value", opt_pretty: true, opt_fields: 'name, memberships.section.name'})
+  .then((result) => {
+    console.log(result);
+        for(var taskCounter = 0; taskCounter < result.data.length; taskCounter++){
+          asanaLifeArray.push([result.data[taskCounter].memberships[0].section.name,result.data[taskCounter].name]);
+        }
+      console.table(asanaLifeArray);
+  });
 
 
 var labelsImport;
@@ -62,6 +88,24 @@ fs.readFile('C:/Users/seanm/OneDrive/Desktop/PaigeTimr/paige-timr/Labels.csv', '
   }
 });
 
+function showLabels(){
+  document.getElementById("task1").getElementsByClassName("taskLabel")[0].innerHTML = task1;
+  document.getElementById("task2").getElementsByClassName("taskLabel")[0].innerHTML = task2;
+  document.getElementById("task3").getElementsByClassName("taskLabel")[0].innerHTML = task3;
+  document.getElementById("task4").getElementsByClassName("taskLabel")[0].innerHTML = task4;
+  document.getElementById("task5").getElementsByClassName("taskLabel")[0].innerHTML = task5;
+  document.getElementById("task6").getElementsByClassName("taskLabel")[0].innerHTML = task6;
+  document.getElementById("task7").getElementsByClassName("taskLabel")[0].innerHTML = task7;
+  document.getElementById("task8").getElementsByClassName("taskLabel")[0].innerHTML = task8;
+  document.getElementsByClassName("taskLabel")[0].style = "visibility: visible;background:black;";
+  document.getElementsByClassName("taskLabel")[1].style = "visibility: visible;background:black;";
+  document.getElementsByClassName("taskLabel")[2].style = "visibility: visible;background:black;";
+  document.getElementsByClassName("taskLabel")[3].style = "visibility: visible;background:black;";
+  document.getElementsByClassName("taskLabel")[4].style = "visibility: visible;background:black;";
+  document.getElementsByClassName("taskLabel")[5].style = "visibility: visible;background:black;";
+  document.getElementsByClassName("taskLabel")[6].style = "visibility: visible;background:black;";
+  document.getElementsByClassName("taskLabel")[7].style = "visibility: visible;background:black;";
+}
 
 window.addEventListener("keydown", checkKeyPressed, false);
 function checkKeyPressed(evt) {
@@ -72,29 +116,14 @@ function checkKeyPressed(evt) {
         subjectState = subject1;
         //work in subtasks where necessary
         task1 = labelsArray[4];
-        console.log(task1);
         task2 = labelsArray[8];
-        console.log(task2);
         task3 = labelsArray[12];
-        console.log(task3);
         task4 = labelsArray[16];
-        console.log(task4);
         task5 = labelsArray[20];
-        console.log(task5);
         task6 = labelsArray[24];
-        console.log(task6);
         task7 = labelsArray[28];
-        console.log(task7);
         task8 = labelsArray[32];
-        console.log(task8);
-        document.getElementById("task1").getElementsByClassName("taskLabel")[0].innerHTML = task1;
-        document.getElementById("task2").getElementsByClassName("taskLabel")[0].innerHTML = task2;
-        document.getElementById("task3").getElementsByClassName("taskLabel")[0].innerHTML = task3;
-        document.getElementById("task4").getElementsByClassName("taskLabel")[0].innerHTML = task4;
-        document.getElementById("task5").getElementsByClassName("taskLabel")[0].innerHTML = task5;
-        document.getElementById("task6").getElementsByClassName("taskLabel")[0].innerHTML = task6;
-        document.getElementById("task7").getElementsByClassName("taskLabel")[0].innerHTML = task7;
-        document.getElementById("task8").getElementsByClassName("taskLabel")[0].innerHTML = task8;
+        showLabels();
     }
     else if (evt.keyCode == "88") { //key X
         subjectState = "Work";
